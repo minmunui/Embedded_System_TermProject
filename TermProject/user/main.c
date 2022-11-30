f4088ad1cbab30aa86b4c600b37660b5c249ccfc
Gunwo Yoon <97718735+Yoongunwo@users.noreply.github.com>
2022-11-30 22:10:34 +0900

    Update main.c

--- TermProject/user/main.c
+++ TermProject/user/main.c
@@ -1,47 +1,175 @@
-
 #include "stm32f10x.h"
-
-#define RCC_APB2ENR (*(volatile unsigned int *)0x40021018)
-#define GPIOC_CRL (*(volatile unsigned int *)0x40011000)
-#define GPIOC_IDR (*(volatile unsigned int *)0x40011008)
-#define GPIOD_ODR (*(volatile unsigned int *)0x4001140C)
-#define GPIOD_IDR (*(volatile unsigned int *)0x40011408)
-#define GPIOD_CRH (*(volatile unsigned int *)0x40011404)
-
-
-#define GPIOD_CRL (*(volatile unsigned int *)0x40011400)
-
-#define GPIOC_BSRR (*(volatile unsigned int *)0x40011010)
-#define GPIOD_BSRR (*(volatile unsigned int *)0x40011410)
-#include "stm32f10x.h"
-#include "stm32f10x.h"
+#include "core_cm3.h"
+#include "misc.h"
 #include "stm32f10x_gpio.h"
-#include "stm32f10x_exti.h"
 #include "stm32f10x_rcc.h"
 #include "stm32f10x_usart.h"
-#include "misc.h"
-#include "core_cm3.h"
+#include "stm32f10x_adc.h"
+#include "lcd.h"
+#include "touch.h"
 
-void RCC_Configure() {
+int color[12] = {WHITE,CYAN,BLUE,RED,MAGENTA,LGRAY,GREEN,YELLOW,BROWN,BRRED,GRAY};
+uint16_t value;
 
-RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
-RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
-RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
-RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+/* function prototype */
+void RCC_Configure(void);
+void GPIO_Configure(void);
+void ADC_Configure(void);
+void NVIC_Configure(void);
+
+//---------------------------------------------------------------------------------------------------
+
+void RCC_Configure(void) // stm32f10x_rcc.h ????
+{
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+  
+// -----------------Port-------------------
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE);
+
+// -----------------Usart-------------------
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+
+// -----------------ADC-------------------
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+
+// -----------------Timer-------------------  
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+  RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
 }
-void GPIO_Configure() {
 
-GPIO_InitTypeDef GPIO_InitStructure;
-GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
-GPIO_InitStructure.GPIO_Pin = (GPIO_Pin_9 | GPIO_Pin_2);
-GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-GPIO_Init(GPIOA, &GPIO_InitStructure);
+void GPIO_Configure(void) // stm32f10x_gpio.h ????
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+  GPIO_InitStructure.GPIO_Pin = (GPIO_Pin_9 | GPIO_Pin_2);
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
 
-GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
-GPIO_InitStructure.GPIO_Pin = (GPIO_Pin_10 | GPIO_Pin_3);
-GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+  GPIO_InitStructure.GPIO_Pin = (GPIO_Pin_10 | GPIO_Pin_3);
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+  // From LCD
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AIN;
+  GPIO_Init(GPIOB, &GPIO_InitStructure);
+  
+  // to activate motor
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_InitStructure.GPIO_Pin = (GPIO_Pin_11 | GPIO_Pin_12 | GPIO_Pin_13);
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOD, &GPIO_InitStructure);
+}
+
+void ADC_Configure() {
+  ADC_InitTypeDef ADC_InitStructure;
+
+  ADC_InitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_None;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfChannel = 1;
+
+  ADC_Init(ADC1, &ADC_InitStructure);
+
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_8, 1, ADC_SampleTime_239Cycles5);
+  ADC_ITConfig(ADC1, ADC_IT_EOC, ENABLE);
+  ADC_Cmd(ADC1 ,ENABLE);
+  ADC_ResetCalibration(ADC1);
+
+  while(ADC_GetResetCalibrationStatus(ADC1)) ;
+  ADC_StartCalibration(ADC1);
+  while(ADC_GetCalibrationStatus(ADC1)) ;
+  ADC_SoftwareStartConvCmd(ADC1, ENABLE) ;
+}
+
+
+void NVIC_Configure(void) { // misc.h
+ 
+  NVIC_InitTypeDef NVIC_InitStructure;
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+
+  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 1;
+  NVIC_Init(&NVIC_InitStructure);
+
+  NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 2;
+  NVIC_Init(&NVIC_InitStructure);
+
+
+  //From LCD
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_0);
+  NVIC_EnableIRQ(ADC1_2_IRQn);
+  NVIC_InitStructure.NVIC_IRQChannel = ADC1_2_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0; // TODO
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+  
+  
+  
+  NVIC_EnableIRQ(TIM2_IRQn);
+  NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0; // TODO
+  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0; // TODO
+  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStructure);
+    
+}
+
+
+void TIM_Configure(void) {
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+  TIM_OCInitTypeDef TIM_OCInitStructure;
+ 
+  int prescale = (uint16_t)(SystemCoreClock / 10000);
+ 
+  TIM_TimeBaseStructure.TIM_Period = 10000;
+  TIM_TimeBaseStructure.TIM_Prescaler = prescale;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Down;
+  
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = 1500; // us
+  TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+ 
+  TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure);
+  TIM_ITConfig(TIM2, TIM_IT_Update, ENABLE);
+  TIM_ARRPreloadConfig(TIM2, ENABLE);
+  TIM_Cmd(TIM2, ENABLE);
+ 
+  TIM_TimeBaseStructure.TIM_Period = 20000 - 1;
+  TIM_TimeBaseStructure.TIM_Prescaler = (uint16_t) (SystemCoreClock / 1000000) - 1;
+  TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure);
+
+  TIM_OC3PreloadConfig(TIM3, TIM_OCPreload_Disable);
+  TIM_ARRPreloadConfig(TIM3, ENABLE);
+  TIM_Cmd(TIM3, ENABLE);
+}
+
+
+void Delay(void) {
+int i;
+for (i = 0; i < 2000000; i++) {}
+}
+
+void ADC1_2_IRQHandler() {
+  if(ADC_GetITStatus(ADC1, ADC_IT_EOC) != RESET ) {
+    value = ADC_GetConversionValue(ADC1);
+   
+    ADC_ClearITPendingBit(ADC1, ADC_IT_EOC);
+  }
 }
 
 void USART1_Configure() {
@@ -70,68 +198,17 @@ void USART2_Configure() {
   USART_ITConfig(USART2, USART_IT_RXNE , ENABLE);
 }
 
-void NVIC_Configure() {
-  NVIC_InitTypeDef NVIC_InitStructure;
-  NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-
-  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x1;
-  NVIC_Init(&NVIC_InitStructure);
-
-  NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0x0;
-  NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0x2;
-  NVIC_Init(&NVIC_InitStructure);
-}
-
-
 char usart1In = 'a';
-char usart2In = 'a';
 
-void delay(void){
-int i = 0;
-for(i=0;i<1000000;i++);
-}
-
-void delay2(void){
-int i = 0;
-for(i=0;i<10000;i++);
-}
-
-void sendDataUART1(uint16_t data) {
-	/* Wait till TC is set */
-	while ((USART1->SR & USART_SR_TC) == 0);
-	USART_SendData(USART1, data);
-}
-
-int myflag;
 void USART1_IRQHandler(void){
   if ( USART_GetITStatus(USART1, USART_IT_RXNE) != RESET ) {
     usart1In = USART_ReceiveData(USART1);
     USART_SendData(USART2, usart1In);
     USART_ClearITPendingBit(USART1, USART_IT_RXNE);
   }
-  
-  uint16_t word;
-  if(USART_GetITStatus(USART1,USART_IT_RXNE)!=RESET){
-    	// the most recent received data by the USART1 peripheral
-    word = USART_ReceiveData(USART1);
-
-        // TODO implement
-    if(word == 'a'){
-      myflag = 0;
-      sendDataUART1('a');
-    }else if(word == 'b') {
-      myflag =1;
-      sendDataUART1('a');
-    }
-    
-        // clear 'Read data register not empty' flag
-      USART_ClearITPendingBit(USART1,USART_IT_RXNE);
-    }
 }
 
+char usart2In = 'a';
 
 void USART2_IRQHandler(void){
   if ( USART_GetITStatus(USART2, USART_IT_RXNE) != RESET ) {
@@ -141,62 +218,229 @@ void USART2_IRQHandler(void){
   }
 }
 
+int cnt = 0;
+uint16_t sub[2] = {1000, 2000};
 
- int main(void){
-  SystemInit();
-  RCC_Configure();
-  GPIO_Configure();
-  USART1_Configure();
-  USART2_Configure();
-  NVIC_Configure();
-  
-  RCC_APB2ENR |= 0x30;
+void delay() {
+  int i;
+  for(i = 0; i < 5000000; ++i) {}
+}
 
-  
-  GPIOC_CRL &= ~0x00F00F00;
-  GPIOC_CRL |= 0x00800800;
-  
-  GPIOD_CRH &= ~0x000FF000;
-  GPIOD_CRH |= 0x00033000;
-  
-  GPIOC_CRL = 0x800800;
-  GPIOD_CRH = 0x00011000;
 
-  GPIOC_BSRR |= 0x240000;
-  GPIOD_BSRR |= 0x8000000;
-  
-  //GPIOD_BSRR |= 0x800;
+void change_pulse(uint16_t pulse){
+  TIM_OCInitTypeDef TIM_OCInitStructure;
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = pulse; // us
+  TIM_OC3Init(TIM3, &TIM_OCInitStructure);
+}
 
-   while(1){
-     if(myflag == 0){ //down
-       GPIOD_ODR  &= ~0x1800;
-       GPIOD_ODR |= 0x800;
-       delay();
-     }
-     else if(myflag == 1){ // up
-          GPIOD_ODR  &= ~0x1800;
-          GPIOD_ODR |= 0x1000;
-          
-          delay();
-     }
-     else if((GPIOD_IDR & 0x800) == 0){ // s1
-        GPIOD_ODR  &= ~0x1800;
-     }
-               
-      if((GPIOC_IDR & 0x04) == 0){ //down
-                GPIOD_ODR  &= ~0x1800;
-        GPIOD_ODR |= 0x800;
-        delay();
-      }
-    
-      else if((GPIOC_IDR & 0x20) == 0){ // up
-                GPIOD_ODR  &= ~0x1800;
-          GPIOD_ODR |= 0x1000;
-          
-          delay();
-      }
-      else if((GPIOD_IDR & 0x800) == 0){ // s1
-        GPIOD_ODR  &= ~0x1800;
-     }
+int time = 0;
+int TICK = 1;
+void timeCheck(){
+  if(time == 60 ){
+    GPIO_SetBits(GPIOD, GPIO_Pin_11);
   }
+  if(time == 120){
+    GPIO_ResetBits(GPIOD, GPIO_Pin_11);
+    time = 0;
+  }
+  LCD_ShowNum(70, 0, time, 10, color[11], color[0]);
+}
+
+
+uint16_t a = 0;
+uint16_t count = 0;
+
+void TIM2_IRQHandler() {
+  if(TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET ) {
+      count ++;
+   
+      time+=TICK;
+      timeCheck();
+      
+      change_pulse(sub[cnt++]);
+      if (cnt == 2)
+        cnt = 0;
+     TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
+  }
+}
+
+
+
+char MSG_TIME[] = "TIME : ";
+char MSG_LUMIN[] = "LUMI : ";
+char MSG_MODE[] = "MODE : ";
+char MSG_AUTO[] = "AUTO  ";
+char MSG_MAN[] = "MANUAL";
+char MSG_LED[] = "LED : ";
+char MSG_BLIND[] = "BLD : ";
+char MSG_ON[] = "ON ";
+char MSG_OFF[] = "OFF";
+
+uint16_t blind_state = 1;
+uint16_t led_state = 1;
+uint16_t auto_mode = 1;
+
+
+uint16_t V_I = 32; // Vertical Interval
+uint16_t H_I = 70; // Horizontal Interval
+
+void LCD_indicate(uint16_t input_time, uint16_t input_lumin, uint16_t is_blind, uint16_t is_led, uint16_t auto_mode) {
+
+  // TIME
+//  LCD_ShowString(0, 0, MSG_TIME, color[11], color[0]);
+//  LCD_ShowNum(H_I, 0, input_time, 10, color[11], color[0]);
+
+  // LUMIN
+  LCD_ShowString(0, V_I, MSG_LUMIN, color[11], color[0]);
+  LCD_ShowNum(H_I, V_I, input_lumin, 10, color[11], color[0]);
+
+  // MODE
+  LCD_ShowString(0, V_I * 2, MSG_MODE, color[11], color[0]);
+  if (auto_mode) {
+    LCD_ShowString(H_I, V_I * 2, MSG_AUTO, color[11], color[0]);
+  }
+  else {
+    LCD_ShowString(H_I, V_I * 2, MSG_MAN, color[11], color[0]);
+  }
+
+  // Blind
+  LCD_ShowString(0, V_I * 4, MSG_BLIND, color[11], color[0]);
+  if (blind_state == 1) {
+    LCD_ShowString(H_I, V_I * 4, MSG_ON, color[11], color[0]);
+  }
+  else {
+    LCD_ShowString(H_I, V_I * 4, MSG_OFF, color[11], color[0]);
+  }
+
+  // LED
+  LCD_ShowString(H_I * 2, V_I * 4, MSG_LED, color[11], color[0]);
+  if ( led_state == 1 ) {
+    LCD_ShowString(H_I * 3, V_I * 4, MSG_ON, color[11], color[0]);
+  }
+  else {
+    LCD_ShowString(H_I * 3, V_I * 4, MSG_OFF, color[11], color[0]);
+  }
+}
+
+
+uint16_t TOUCH_WIDTH = 20;
+
+void activate_blind() {
+  blind_state = 1;
+  // TODO 블라인드 치다
+}
+
+void remove_blind() {
+  blind_state = 0;
+  // TODO 블라인드 걷다
+}
+
+void control_blind() {
+  if ( auto_mode ) {
+    auto_mode = !auto_mode;
+  }
+  if ( blind_state == 0 ) {
+    blind_state = 1;
+    activate_blind();
+  }
+  else {
+    blind_state = 0;
+    remove_blind();
+  }
+}
+
+void on_LED() {
+  led_state = 1;
+  GPIO_SetBits(GPIOD, GPIO_Pin_11);
+}
+
+void off_LED() {
+  led_state = 0;
+  GPIO_ResetBits(GPIOD, GPIO_Pin_11);
+}
+
+void control_LED() {
+  if ( auto_mode ) {
+    auto_mode = !auto_mode;
+  }
+  led_state = !led_state;
+  if ( led_state == 0 ) {
+    on_LED();
+  }
+  else {
+    off_LED();
+  }
+}
+
+void process_touch(uint16_t x, uint16_t y) {
+
+  // BLIND sense
+  if ( H_I * 0 <= x && x <= H_I * 1 && V_I * 4 - TOUCH_WIDTH <= y && y <= V_I * 4 + TOUCH_WIDTH ) {
+    control_blind();
+  }
+
+  // LED sense
+  if ( H_I * 1 <= x && x <= H_I * 2 && V_I * 4- TOUCH_WIDTH <= y && y <= V_I * 4 + TOUCH_WIDTH ) {
+    control_LED();
+  }
+
+  // MODE sense
+  if ( H_I * 0 <= x && x <= H_I * 1 && V_I * 2 - TOUCH_WIDTH <= y && y <= V_I * 2 + TOUCH_WIDTH ) {
+    auto_mode = !auto_mode;
+  }
+
+}
+
+
+int main(void)
+{
+    SystemInit();
+    RCC_Configure();
+    GPIO_Configure();
+    ADC_Configure();
+    NVIC_Configure();
+    USART1_Configure();
+    USART2_Configure();
+    TIM_Configure();
+
+    LCD_Init();
+    Touch_Configuration();
+    //Touch_Adjust();
+    LCD_Clear(WHITE);
+    
+      LCD_ShowString(0, 0, MSG_TIME, color[11], color[0]);
+
+  /*
+    char msg[] = "WED_Team08";
+    LCD_ShowString(0, 0, msg, color[11], color[0] );
+*/
+    uint16_t x, y;
+   
+    while (1) {
+//      GPIO_SetBits(GPIOD, GPIO_Pin_11);
+//      GPIOD->BSRR |= GPIO_Pin_11;
+//      GPIO_SetBits(GPIOD, GPIO_Pin_12);
+//      GPIO_SetBits(GPIOD, GPIO_Pin_13);
+//      GPIO_ResetBits(GPIOD, GPIO_Pin_13);
+
+      Touch_GetXY(&x, &y, 1);
+      Convert_Pos(x, y, &x, &y);
+      process_touch(x, y);
+      
+      /*
+      LCD_ShowNum(0, 32, value, 10,color[11], color[0]);
+      LCD_DrawCircle(x, y, 6);
+*/
+      
+      LCD_indicate(time, 1200, auto_mode, blind_state, led_state);
+      LCD_ShowNum(0, V_I * 5, x, 10,color[11], color[0]);
+      LCD_ShowNum(0, V_I * 6, y, 10,color[11], color[0]);
+      
+
+    }
+       
+    return 0;
 }
